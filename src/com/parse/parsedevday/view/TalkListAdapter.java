4ecf750fb4a44e7ceaaae308da02ad62e4f8c86c
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2013-11-06 07:00:21 +0100

    fixed typo

--- src/com/parse/parsedevday/view/TalkListAdapter.java
+++ src/com/parse/parsedevday/view/TalkListAdapter.java
@@ -92,7 +92,7 @@ public class TalkListAdapter extends ArrayAdapter<Talk> {
             speakerimage.loadInBackground(new GetDataCallback() {
                 @Override
                 public void done(byte[] data, ParseException e) {
-                    if (data != null && e == null) {
+                    if (data != null) {
                         Log.i("ParseImageView", "Fetched! Data length: " + data.length);
                     }else if(e!=null && e.getMessage()!=null){
                         Log.e("ParseImageView", "exception: " + e.getMessage());

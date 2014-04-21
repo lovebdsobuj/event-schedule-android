6cf1ce111dd8946cbf9102d34ce2c870df1845db
Steven Mulder <steven.mulder@gmail.com>
2014-04-21 18:18:45 +0200

    Created view holder for talk list items.
    Lets my have my cake and eat it too. More performance and more structure!

--- src/main/java/com/xebia/xebicon2014/view/TalkListAdapter.java
+++ src/main/java/com/xebia/xebicon2014/view/TalkListAdapter.java
@@ -1,106 +1,32 @@
 package com.xebia.xebicon2014.view;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-import com.parse.GetDataCallback;
-import com.parse.ParseException;
-import com.parse.ParseImageView;
 import com.xebia.xebicon2014.R;
-import com.xebia.xebicon2014.model.Favorites;
 import com.xebia.xebicon2014.model.Talk;
 
 /**
  * An ArrayAdapter to handle a list of Talks.
  */
 public class TalkListAdapter extends ArrayAdapter<Talk> {
+
     public TalkListAdapter(Context context) {
         super(context, 0);
     }
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-        // This is all pretty standard code for setting up an Android view.
 
         if (view == null) {
             view = View.inflate(getContext(), R.layout.list_item_talk, null);
         }
 
         final Talk talk = getItem(position);
+        ((TalkListItemView) view).showTalk(talk);
 
-        TextView startDateView = (TextView) view.findViewById(R.id.start_date);
-        startDateView.setText(DateFormat.getTimeFormat(getContext()).format(talk.getSlot().getStartTime()));
-
-        TextView titleView = (TextView) view.findViewById(R.id.title);
-        TextView roomView = (TextView) view.findViewById(R.id.room);
-
-        ParseImageView speakerimage = (ParseImageView) view.findViewById(R.id.speakerimage);
-        // The placeholder will be used before and during the fetch, to be
-        // replaced by the fetched image
-        // data.
-
-        titleView.setText(talk.getTitle());
-        roomView.setText(talk.getRoom().getName());
-
-        final ImageButton favoriteButton = (ImageButton) view.findViewById(R.id.favorite_button);
-        if (Favorites.get().contains(talk)) {
-            favoriteButton.setImageResource(R.drawable.light_rating_important);
-        } else {
-            favoriteButton.setImageResource(R.drawable.light_rating_not_important);
-        }
-        favoriteButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                Favorites favorites = Favorites.get();
-                if (favorites.contains(talk)) {
-                    favorites.remove(talk);
-                    favoriteButton.setImageResource(R.drawable.light_rating_not_important);
-                } else {
-                    favorites.add(talk);
-                    favoriteButton.setImageResource(R.drawable.light_rating_important);
-                }
-                favorites.save(getContext());
-            }
-        });
-        favoriteButton.setFocusable(false);
-
-        if (talk.isAlwaysFavorite()) {
-            favoriteButton.setVisibility(View.GONE);
-            speakerimage.setVisibility(View.INVISIBLE);
-        } else {
-            favoriteButton.setVisibility(View.VISIBLE);
-            speakerimage.setVisibility(View.VISIBLE);
-        }
-
-        if (talk.isAlwaysFavorite()) {
-            view.setBackgroundColor(Color.rgb(255, 255, 245));
-        } else {
-            view.setBackgroundColor(Color.rgb(245, 245, 245));
-        }
-        speakerimage.setPlaceholder(this.getContext().getResources().getDrawable(R.drawable.ic_launcher));
-        if (talk.getSpeakers() != null && !talk.getSpeakers().isEmpty()
-                && talk.getSpeakers().get(0).getPhoto() != null) {
-            speakerimage.setParseFile(talk.getSpeakers().get(0).getPhoto());
-            speakerimage.loadInBackground(new GetDataCallback() {
-                @Override
-                public void done(byte[] data, ParseException e) {
-                    if (data != null && e == null) {
-                        Log.i("ParseImageView", "Fetched! Data length: " + data.length);
-                    }else if(e!=null && e.getMessage()!=null){
-                        Log.e("ParseImageView", "exception: " + e.getMessage());
-                    }
-                }
-            });
-        } else {
-            speakerimage.setParseFile(null);
-        }
         return view;
     }
 }
---
+++ src/main/java/com/xebia/xebicon2014/view/TalkListItemView.java
@@ -0,0 +1,147 @@
+package com.xebia.xebicon2014.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.text.format.DateFormat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseImageView;
+import com.xebia.xebicon2014.R;
+import com.xebia.xebicon2014.model.Favorites;
+import com.xebia.xebicon2014.model.Talk;
+
+/**
+ * View group for talk list items. Lets me have my cake and eat it too.
+ * <p/>
+ * Created by steven on 21-4-14.
+ */
+public class TalkListItemView extends RelativeLayout {
+
+    private TextView mTitleView;
+    private TextView mStartDateLabel;
+    private TextView mStartDateView;
+    private TextView mRoomLabel;
+    private TextView mRoomView;
+    private ParseImageView mSpeakerImage;
+    private ImageButton mFavoriteButton;
+
+    private int mPurple;
+    private java.text.DateFormat mTimeFormat;
+
+    private Talk mTalk;
+
+    public TalkListItemView(Context context) {
+        super(context);
+    }
+
+    public TalkListItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @SuppressLint("NewApi")
+    public TalkListItemView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mTitleView = (TextView) findViewById(R.id.title);
+        mStartDateLabel = (TextView) findViewById(R.id.start_date_label);
+        mStartDateView = (TextView) findViewById(R.id.start_date);
+        mRoomLabel = (TextView) findViewById(R.id.room_label);
+        mRoomView = (TextView) findViewById(R.id.room);
+        mSpeakerImage = (ParseImageView) findViewById(R.id.speakerimage);
+        mFavoriteButton = (ImageButton) findViewById(R.id.favorite_button);
+        mFavoriteButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                toggleFavorite();
+            }
+        });
+
+        // load some context-related things
+        mPurple = getContext().getResources().getColor(R.color.xcs_purple);
+        mTimeFormat = DateFormat.getTimeFormat(getContext());
+    }
+
+    public void showTalk(final Talk talk) {
+        mTalk = talk;
+        this.setBackgroundColor(talk.isAlwaysFavorite() ? mPurple : Color.TRANSPARENT);
+        updateTextViews(talk);
+        updateFavoriteButton(talk);
+        updateSpeakerImage(talk);
+    }
+
+    private void updateSpeakerImage(final Talk talk) {
+        if (talk.isAlwaysFavorite()) {
+            mSpeakerImage.setVisibility(View.INVISIBLE);
+        } else {
+            mSpeakerImage.setVisibility(View.VISIBLE);
+
+            if (talk.getSpeakers() != null && !talk.getSpeakers().isEmpty()
+                    && talk.getSpeakers().get(0).getPhoto() != null) {
+                mSpeakerImage.setParseFile(talk.getSpeakers().get(0).getPhoto());
+                mSpeakerImage.loadInBackground(new GetDataCallback() {
+                    @Override
+                    public void done(byte[] data, ParseException e) {
+                        if (data != null && e == null) {
+                            Log.i("ParseImageView", "Fetched! Data length: " + data.length);
+                        } else if (e != null && e.getMessage() != null) {
+                            Log.e("ParseImageView", "exception: " + e.getMessage());
+                        }
+                    }
+                });
+            } else {
+                mSpeakerImage.setParseFile(null);
+            }
+        }
+    }
+
+    private void updateFavoriteButton(final Talk talk) {
+        if (talk.isAlwaysFavorite()) {
+            mFavoriteButton.setVisibility(View.GONE);
+        } else {
+            mFavoriteButton.setVisibility(View.VISIBLE);
+            mFavoriteButton.setImageResource(Favorites.get().contains(talk) ? R.drawable
+                    .light_rating_important : R.drawable.light_rating_not_important);
+            mFavoriteButton.setFocusable(false);
+        }
+    }
+
+    private void updateTextViews(final Talk talk) {
+        mTitleView.setText(talk.getTitle());
+        mStartDateView.setText(mTimeFormat.format(talk.getSlot().getStartTime()));
+        mRoomView.setText(talk.getRoom().getName());
+
+        int textColor = talk.isAlwaysFavorite() ? Color.WHITE : mPurple;
+        mTitleView.setTextColor(textColor);
+        mStartDateLabel.setTextColor(textColor);
+        mStartDateView.setTextColor(textColor);
+        mRoomLabel.setTextColor(textColor);
+        mRoomView.setTextColor(textColor);
+    }
+
+    private void toggleFavorite() {
+        if (null == mTalk) {
+            return;
+        }
+
+        Favorites favorites = Favorites.get();
+        if (favorites.contains(mTalk)) {
+            favorites.remove(mTalk);
+            mFavoriteButton.setImageResource(R.drawable.light_rating_not_important);
+        } else {
+            favorites.add(mTalk);
+            mFavoriteButton.setImageResource(R.drawable.light_rating_important);
+        }
+        favorites.save(getContext());
+    }
+}

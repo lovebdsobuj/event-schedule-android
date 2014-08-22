f448245855e975d3cdc75a383d067b659ae1d15e
Jeroen Willemsen <jwillemsen@xebia.com>
2014-08-22 11:09:40 +0200

    for demo purposes: use a synchronous find

--- src/main/java/com/xebia/xebicon2014/util/DataStore.java
+++ src/main/java/com/xebia/xebicon2014/util/DataStore.java
@@ -12,6 +12,7 @@ import com.xebia.xebicon2014.model.Event;
 import com.parse.ParseException;
 
 import java.util.List;
+import java.util.Set;
 
 public class DataStore {
 
@@ -27,24 +28,21 @@ public class DataStore {
         //check from the local datastore whether an event is saved
         ParseQuery<Event> query = ParseQuery.getQuery("Event");
         query.fromLocalDatastore();
-        query.findInBackground(new FindCallback<Event>() {
-
-            @Override
-            public void done(List<Event> events, ParseException e) {
-                if(e==null){
-                    if(events.size()>0){
-                        selectedEvent = events.get(0);
-                        Log.d(TAG, "event loaded");
-                    }else{
-                        Log.d(TAG, "no event found");
-                    }
-                }else{
-                    if(e.getMessage()!=null){
-                        Log.e(TAG, e.getMessage());
-                    }
-                }
+        try {
+            List<Event> events = query.find();
+            if (events.size() > 0) {
+                selectedEvent = events.get(0);
+                Log.d(TAG, "event loaded");
+            } else {
+                Log.d(TAG, "no event found");
             }
-        });
+
+        } catch (ParseException e) {
+            if (e.getMessage() != null) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+
     }
 
 

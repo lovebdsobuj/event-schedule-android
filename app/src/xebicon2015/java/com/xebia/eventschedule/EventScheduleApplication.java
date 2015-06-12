7b8c0ce32748a9018e81bc13addc51c3835fa6f3
Jeroen Willemsen <jwillemsen@xebia.com>
2015-06-12 16:12:21 +0200

    parse_push_notification: enabled the basics

--- app/src/xebicon2015/java/com/xebia/eventschedule/EventScheduleApplication.java
+++ app/src/xebicon2015/java/com/xebia/eventschedule/EventScheduleApplication.java
@@ -1,17 +1,39 @@
 package com.xebia.eventschedule;
 
+import android.util.Log;
+
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 import com.xebia.eventschedule.util.BaseEventScheduleApp;
 
 public class EventScheduleApplication extends BaseEventScheduleApp {
 
     @Override
-    protected String getParseApplicationId() {
-        return "egcq81G2Yxf1C8yhAFtW5aOf7UkO5BsNB2mOyj2t";
+    public void onCreate() {
+        super.onCreate();
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 
     @Override
+    protected String getParseApplicationId() {
+        return "0ykqcwEDNSAzXQGw8rpmAw8zzMKEDuLIz2Sj31fu";
+    }
+    //egcq81G2Yxf1C8yhAFtW5aOf7UkO5BsNB2mOyj2t
+
+    @Override
     protected String getParseClientKey() {
-        return "skKIoBIE59IwNSzN6q1vrFRVn72TofyIkVGNlHLp";
+        return "8XvCbouEfWFF775SYuTo28MxYvOxQoA4HeADScFG";
+        //skKIoBIE59IwNSzN6q1vrFRVn72TofyIkVGNlHLp
     }
 
     @Override

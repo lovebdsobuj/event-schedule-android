e10268962c484bc498fdea8e4c8f6893c63820ef
Steven Mulder <steven.mulder@gmail.com>
2015-09-20 12:19:29 +0200

    Add resources for TestWork Conf build flavor

---
+++ app/src/tstworks/java/com/xebia/eventschedule/EventScheduleApplication.java
@@ -0,0 +1,21 @@
+package com.xebia.eventschedule;
+
+import com.xebia.eventschedule.util.BaseEventScheduleApp;
+
+public class EventScheduleApplication extends BaseEventScheduleApp {
+
+    @Override
+    protected String getParseApplicationId() {
+        return "egcq81G2Yxf1C8yhAFtW5aOf7UkO5BsNB2mOyj2t";
+    }
+
+    @Override
+    protected String getParseClientKey() {
+        return "skKIoBIE59IwNSzN6q1vrFRVn72TofyIkVGNlHLp";
+    }
+
+    @Override
+    public String getParseEventId() {
+        return "mIjGuRHJbp";
+    }
+}
